2791ce9752c8e9b0e77ab1bcad7020f93178834d
thiephn-2606 <huynh.nhat.thiep@sun-asterisk.com>
2021-07-19 14:36:47 +0700

    bai 3

--- check_time_ot.rb
+++ check_time_ot.rb
@@ -3,45 +3,39 @@ require 'time'
 
 class CheckTimeOt
 
-	def input_time
-		begin
-			puts "Please enter dob in hh:mm format;"
-			puts "Time check in "
-			time_checkin = gets.chomp
-			puts "Time check out"
-			time_checkout = gets.chomp
-			@time_checkin = DateTime.strptime(time_checkin, '%H:%M').to_time
-			@time_checkout = DateTime.strptime(time_checkout, '%H:%M').to_time
-		
-		rescue Exception => e
-			puts e.message
-		end
-	end
+  def input_time
+    begin
+      puts "Please enter dob in hh:mm format;"
+      puts "Time check in "
+      time_checkin = gets.chomp
+      puts "Time check out"
+      time_checkout = gets.chomp
+      @time_checkin = Time.strptime(time_checkin, '%H:%M').to_time
+      @time_checkout = Time.strptime(time_checkout, '%H:%M').to_time
 
-	def time_OT
-		(@time_checkout - @time_checkin) /3600
-	end
+    rescue ArgumentError => e
+      puts e.message
+    end
+  end
 
-	def check_time_OT
-		ot_time = @time_checkin - @time_checkout
+  def time_OT
+    (@time_checkout - @time_checkin) /3600
+  end
 
-		case time_OT
-
-		when 0..4
-			if @time_checkout > DateTime.strptime("21:00", '%H:%M').to_time
-				puts "OT: #{time_OT}, Lunch: N, Dinner: Y"
-			else 
-				puts "OT: #{time_OT}, Lunch: N, Dinner: N"
-			end
-		when 4..12
-			if(@time_checkin.hour..@time_checkout.hour).to_a.include?(12) && (@time_checkin.hour..@time_checkout.hour).to_a.include?(13) 
-				puts "OT: #{(time_OT) -1 }, Lunch: y, Dinner: N"  
-			else
-				puts "OT: #{(time_OT)}, Lunch: N, Dinner: N" 
-			end
-		else
-			puts "OT: #{time_OT}, Lunch: N, Dinner: N"
-		end
+  def check_time_OT
+    ot_time = @time_checkin - @time_checkout
+    if @time_checkin >= @time_checkout
+      puts "please enter check_out time greater than check_in"
+    else
+      if time_OT > 3 &&  @time_checkout > Time.strptime("21:00", '%H:%M').to_time
+        puts "OT: #{time_OT}, Lunch: N, Dinner: Y"
+      elsif time_OT > 4 && (@time_checkin.hour..@time_checkout.hour).to_a.include?(12) &&
+        (@time_checkin.hour..@time_checkout.hour).to_a.include?(13)
+        puts "OT: #{(time_OT) -1 }, Lunch: y, Dinner: N"
+      else
+        puts "OT: #{(time_OT)}, Lunch: N, Dinner: N"
+      end
+    end
   end
 end
 
